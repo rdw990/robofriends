33810754cbbced602b9468351289d90fd2d39452
Richard Whiteside <rwhiteside4@gmail.com>
2019-04-29 14:47:01 +0100

    scroll finish

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Scroll = (props) => {
   return (
     <div style={{overflowY: "scroll", border: "5px solid black", height:"800px"}}>
-      {props.children}
+       {props.children}
     </div>
   ) 
 }
